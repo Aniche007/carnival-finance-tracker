d72bac22d80083e30e71ab05f6712f679ee8f6af
Aniche007 <kalki6969ad@gmail.com>
2025-11-05 22:05:51 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -19,9 +19,10 @@ with app.app_context():
     desk1 = make_user("desk1", "desk1pass", "desk")
     desk2 = make_user("desk2", "desk2pass", "desk")
     desk3 = make_user("desk3", "desk3pass", "desk")
+    desk4 = make_user("desk4", "desk4pass", "desk")
 
     # Save to DB
-    db.session.add_all([admin, desk1, desk2, desk3])
+    db.session.add_all([admin, desk1, desk2, desk3,desk4])
     db.session.commit()
 
     print("✅ Database initialized successfully!")
@@ -30,3 +31,4 @@ with app.app_context():
     print(" - desk1 / desk1pass")
     print(" - desk2 / desk2pass")
     print(" - desk3 / desk3pass")
+    print(" - desk4 / desk4pass")
